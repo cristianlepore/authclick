a85a9a9e3a3602f0a42a9c33afd0060a8efb90dd
crislepo <clepore@localhost.localdomain>
2019-08-03 12:29:06 +0200

    Versione 0.2.2
    Bug fix sulla barra di caricamento dei file.

--- form.php
+++ form.php
@@ -521,45 +521,6 @@ function myFunction() {
 
 }
 
-// USATA PER LA BARRA DI CARICAMENTO DEL FILE
-function _(el) {
-  return document.getElementById(el);
-}
-
-function uploadFile() {
-  var file = _("file").files[0];
-  // alert(file.name+" | "+file.size+" | "+file.type);
-  var formdata = new FormData();
-  formdata.append("file", file);
-  var ajax = new XMLHttpRequest();
-  ajax.upload.addEventListener("progress", progressHandler, false);
-  ajax.addEventListener("load", completeHandler, false);
-  ajax.addEventListener("error", errorHandler, false);
-  ajax.addEventListener("abort", abortHandler, false);
-  ajax.open("POST", "file_upload_parser.php");
-  ajax.send(formdata);
-}
-
-function progressHandler(event) {
-  _("loaded_n_total").innerHTML = "Caricati " + event.loaded + " bytes di " + event.total;
-  var percent = (event.loaded / event.total) * 100;
-  _("progressBar").value = Math.round(percent);
-  _("status").innerHTML = Math.round(percent) + "% uploaded... please wait";
-}
-
-function completeHandler(event) {
-  _("status").innerHTML = event.target.responseText;
-  _("progressBar").value = 0; //wil clear progress bar after successful upload
-}
-
-function errorHandler(event) {
-  _("status").innerHTML = "Upload Failed";
-}
-
-function abortHandler(event) {
-  _("status").innerHTML = "Upload Aborted";
-}
-
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction(); myFunction_progressBar();};
 
